58fa70b5d377ea4563917529bc06661e14d16bea
Unknown <lorenz.hufe@web.de>
2017-05-28 15:49:00 +0200

    external ip

--- client/client.py
+++ client/client.py
@@ -19,7 +19,8 @@ class Client:
             port = int(raw_input("Enter you Port here : "))
         else:
             ip = '192.168.178.32'   # Lubuntu
-            ip = '87.122.83.113'    # Fritz Box external
+            ip = '87.123.138.95'    # Fritz Box external
+
             port = 12345
 
 
